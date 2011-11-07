00b725604a674b4d071ef1fd86e5bc76de79a39b
Guilherme Baufaker <baufaker@gmail.com>
2011-11-07 18:39:54 -0200

    primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BancoDeIdeias::Application.config.secret_token = '02b1ffd7adb3bdac452436c40cf6c01a45a7f4e85915a582f1047a2cf095d82715c045cb2c90441a7e1768b707814a51ce1003e0268efce77ca678075ee1c75b'
