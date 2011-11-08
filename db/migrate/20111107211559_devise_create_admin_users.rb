3b77ce559b7209b90761a4860fad5937391fae52
Guilherme Baufaker <baufaker@gmail.com>
2011-11-07 22:03:26 -0200

    mudei o nome do usuario

--- db/migrate/20111107211559_devise_create_admin_users.rb
+++ db/migrate/20111107211559_devise_create_admin_users.rb
@@ -16,7 +16,7 @@ class DeviseCreateAdminUsers < ActiveRecord::Migration
     end
 
     # Create a default user
-    AdminUser.create!(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password')
+    AdminUser.create!(:email => 'bancodeideias@bancodeideias.com', :password => 'gayporn', :password_confirmation => 'gayporn')
 
     add_index :admin_users, :email,                :unique => true
     add_index :admin_users, :reset_password_token, :unique => true
